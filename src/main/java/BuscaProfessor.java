6ca3b30d2ac1ea93bd176c6e4acb046f975b6e8f
Matheu Chagas <matheusilva431@gmail.com>
2022-08-25 21:12:17 -0300

    Adicionando a classe BuscaProfessor.java e adicionando dependencia gson em pom.xml

---
+++ src/main/java/BuscaProfessor.java
@@ -0,0 +1,22 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+public class BuscaProfessor {
+
+    ProfessorService professorService;
+
+    public BuscaProfessor(ProfessorService service){
+        this.professorService = service;
+    }
+
+    public Professor buscaProfessor(int id){
+        String professorJson = professorService.busca(id);
+        JsonObject jsonObject = JsonParser.parseString(professorJson).getAsJsonObject();
+
+        return new Professor(
+                jsonObject.get("nomeProfessor").getAsString(),
+                jsonObject.get("horarioAtendimento").getAsString(),
+                jsonObject.get("periodo").getAsString()
+                );
+    }
+
+}
