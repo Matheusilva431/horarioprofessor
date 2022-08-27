0b31ebda049f26f2d93f76571a47e730d5c073fd
Matheu Chagas <matheusilva431@gmail.com>
2022-08-26 21:01:31 -0300

    Criando a classe ProfessorConst.java e deletendo a classe MockProfessorService.java

--- src/test/java/MockProfessorService.java
+++
@@ -1,7 +0,0 @@
-public class MockProfessorService implements ProfessorService{
-
-    @Override
-    public String busca(int id){
-        
-    }
-}
---
+++ src/test/java/ProfessorConst.java
@@ -0,0 +1,18 @@
+public class ProfessorConst {
+
+    public static String rosimara =
+                  "{ \"nomeProfessor\": \"Rosimara\", \n " +
+                    "\"horarioAtendimento\"Segunda 17:30-19:30\n " +
+                    "\"periodo\": \"Noturno\" }";
+
+    public static String christopher =
+            "{ \"nomeProfessor\": \"Christopher\", \n " +
+                    "\"horarioAtendimento\"Terça 17:30-19:10\n " +
+                    "\"periodo\": \"Noturno\" }";
+
+    public static String samuel =
+            "{ \"nomeProfessor\": \"Samuel\", \n " +
+                    "\"horarioAtendimento\"Quinta 15:30-16:30\n " +
+                    "\"periodo\": \"Diurno\" }";
+
+}
